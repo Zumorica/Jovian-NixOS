d5a3fb4eedf5438390501c40b7135e207a268fd3
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-04-23 03:48:51 +0200

    Update mangohud

--- pkgs/mangohud/default.nix
+++ pkgs/mangohud/default.nix
@@ -6,8 +6,8 @@ mangohud.overrideAttrs (old: {
     owner = "flightlessmango";
     repo = "MangoHud";
 
-    # As shipped in SteamOS 3 repo 0.6.8.r17.gebb0f96
-    rev = "ebb0f969dea2c5df1600e6e299cd5665920e0020";
-    hash = "sha256-+luu/YjSNdhUl3TDyNrsvSiZOL3Jpfx0wv3MPe+vtyQ=";
+    # As shipped in SteamOS 3 repo 0.6.8.r148.g070fa1b
+    rev = "070fa1be842479942b55505688bcb06e86875075";
+    hash = "sha256-x9eO05pLNAH82ndXjVZfyN0G99ueUg3YcehjfabCU8k=";
   };
 })
